d55023a5739df70e880d2953307004f89ec47378
dayooo <bahtundey0044@gmail.com>
2026-01-14 11:54:17 +0100

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),]
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      manifest: {
+        name: 'VoteX',
+        short_name: 'VoteX',
+        description: 'Voting  application',
+        theme_color: '#2c5ae3ff',
+        background_color: '#053627ff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon1.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]})
